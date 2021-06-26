d4986566dcb7a4c457cccfd9a251773aeb831408
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-06-26 14:05:35 +0430

    first commit

---
+++ test01.py
@@ -0,0 +1,19 @@
+# rule for name of variable 
+# use letters, numbers, underscore
+# but you can not start with numbers
+# you can not use space
+# Avoid using Python keywords and function names as variable names
+message = "Hello World!" 
+print(message)
+
+
+message = 'Hello "Python"!'
+print(message) 
+print(message.lower())
+# Strings
+# A string is simply a series of characters.
+
+message = "hello arnika and niloufar!"
+print(message.title())
+
+print(message.upper())
---
+++ tets02.py
@@ -0,0 +1,11 @@
+name = "Arnika" 
+last_name = "Zainolabediny"
+
+message = name+" "+last_name
+print(message)
+
+message = " pyhton "
+print(len(message))
+print(len(message.lstrip())) #cleaning left space
+print(len(message.rstrip())) #cleaning right space
+print(len(message.strip()))
