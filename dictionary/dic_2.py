97fe47dd047f7bdec31ca82d7efa624f2ccd9ac0
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-07-17 13:59:59 +0430

    26Tir

---
+++ dictionary/dic_2.py
@@ -0,0 +1,8 @@
+dic_2 = {
+    "Arnica": "09333565263",
+    "Jafar": "09374568525"
+}
+
+dic_2["Jafar"] = "093632588659" # edit an element
+dic_2["Changiz Khan"] = "09354321245" # add an element
+print(dic_2)
---
+++ dictionary/dic_3.py
@@ -0,0 +1,9 @@
+dic_2 = {
+    "Arnica": "09333565263",
+    "Jafar": "09374568525"
+}
+
+inp1 = input("create a contact: ")
+inp2 = input("phone number: ")
+dic_2[inp1.title()] = inp2
+print(dic_2)
